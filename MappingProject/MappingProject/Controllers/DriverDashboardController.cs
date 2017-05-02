3f35b7b668e5d94e5046761963e75853831cb7fc
Muna Bhai <Muna Bhai@RAO>
2017-05-02 18:15:04 +0500

    front end files

--- MappingProject/MappingProject/Controllers/DriverDashboardController.cs
+++ MappingProject/MappingProject/Controllers/DriverDashboardController.cs
@@ -86,7 +86,7 @@ namespace MappingProject.Controllers
 
             return View();
         }
-C:\Users\Muna Bhai\Source\Repos\MappingProj\MappingProject\MappingProject\js\
+
         
         public ActionResult FleetHistory()
         {
